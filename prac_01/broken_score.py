cd04923e364e56e57027dd0ed11189f2aa86e7f3
jc428178 <andrew.tomlin@my.jcu.edu.au>
2017-08-17 10:10:49 +1000

    prac_03

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -3,13 +3,23 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
 
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("bad")
+def main():
+    # get score in percentage and show it as a grade
+    score = float(input("Enter score: "))
+    print(determine_status(score))
+
+
+def determine_status(score):
+    # find out the grade using the score
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
