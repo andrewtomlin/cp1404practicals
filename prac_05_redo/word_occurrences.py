bb9d316905f71bcde8f0eeaad06b6eac17a248c3
andrewtomlin <andrew.tomlin@my.jcu.edu.au>
2017-08-31 11:08:27 +1000

    Prac_05

--- prac_05_redo/word_occurrences.py
+++ prac_05_redo/word_occurrences.py
@@ -1,19 +1,19 @@
 
-unique_words = {}
+words_used = {}
 text = input("Text: ")
 
-words = text.split()
-for word in words:
-    frequency = unique_words.get(word, 0)
+all_words_used = text.split()
+for word in all_words_used:
+    frequency = words_used.get(word, 0)
 
     if frequency is None:
-        unique_words[word] = 1
+        words_used[word] = 1
     else:
-        unique_words[word] = frequency + 1
+        words_used[word] = frequency + 1
 
 
-words = list(unique_words.keys())
+all_words_used = list(words_used.keys())
 
-max_length = max((len(word) for word in words))
-for word in words:
-    print("{:{}} : {}".format(word, max_length, unique_words[word]))
+max_length = max((len(word) for word in all_words_used))
+for word in all_words_used:
+    print("{:{}} : {}".format(word, max_length, words_used[word]))
